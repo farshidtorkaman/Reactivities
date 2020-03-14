fd42225d5ac003df6853e017b8a9d314a9ff7268
Farshid Torkaman <farshidtorkaman@outlook.com>
2020-03-14 13:43:50 +0330

    axios implementation and add loading component

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,15 +1,19 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect, SyntheticEvent } from 'react';
 import { Container } from 'semantic-ui-react';
 import { IActivity } from '../models/activity';
 import { Navbar } from '../../features/nav/Navbar';
 import { ActivityDashboard } from '../../features/activities/dashboard/ActivityDashboard'
+import agent from '../api/agent';
+import { LoadingComponent } from './LoadingComponent'
 
 const App = () => {
 
   const [activities, setActivities] = useState<IActivity[]>([]);
   const [selectedActivity, setSelectedActivity] = useState<IActivity | null>(null);
   const [editMode, setEditMode] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [submitting, setSubmitting] = useState(false);
+  const [target, setTarget] = useState('');
 
   const handleSelectActivity = (id: string) => {
     setSelectedActivity(activities.filter(f => f.id === id)[0]);
@@ -22,48 +26,62 @@ const App = () => {
   }
 
   const handleCreateActivity = (activity: IActivity) => {
-    setActivities([...activities, activity]);
-    setEditMode(false);
-    setSelectedActivity(activity);
+    setSubmitting(true)
+    agent.Activities.create(activity).then(() => {
+      setActivities([...activities, activity]);
+      setEditMode(false);
+      setSelectedActivity(activity);
+    }).then(() => setSubmitting(false))
   }
 
   const handleEditActivity = (activity: IActivity) => {
-    setActivities([...activities.filter(f => f.id !== activity.id), activity]);
-    setEditMode(false);
-    setSelectedActivity(activity);
+    setSubmitting(true)
+    agent.Activities.update(activity).then(() => {
+      setActivities([...activities.filter(f => f.id !== activity.id), activity]);
+      setEditMode(false);
+      setSelectedActivity(activity);
+    }).then(() => setSubmitting(false))
   }
 
-  const handleDeleteActivity = (id: string) => {
-    setActivities([...activities.filter(f => f.id !== id)])
+  const handleDeleteActivity = (event: SyntheticEvent<HTMLButtonElement>,id: string) => {
+    setSubmitting(true)
+    setTarget(event.currentTarget.name)
+    agent.Activities.delete(id).then(() => {
+      setActivities([...activities.filter(f => f.id !== id)])
+    }).then(() => setSubmitting(false))
   }
 
   useEffect(() => {
-    axios.get<IActivity[]>('http://localhost:5000/api/activities')
-      .then((response) => {
+    agent.Activities.list()
+      .then(response => {
         let activities: IActivity[] = [];
-        response.data.forEach(activity => {
+        response.forEach(activity => {
           activity.date = activity.date.split('.')[0];
           activities.push(activity);
         })
         setActivities(activities);
-      })
+      }).then(() => setLoading(false))
   }, []);
+
+  if(loading) return <LoadingComponent content='Loading Activities...' inverted />
 
   return (
     <>
-      <Navbar openCreateForm={handleOpenCreateForm}/>
+      <Navbar openCreateForm={handleOpenCreateForm} />
 
       <Container style={{ marginTop: 70 }}>
-        <ActivityDashboard 
-          activities={activities} 
-          selectActivity={handleSelectActivity} 
-          selectedActivity={selectedActivity} 
-          editMode={editMode} 
+        <ActivityDashboard
+          activities={activities}
+          selectActivity={handleSelectActivity}
+          selectedActivity={selectedActivity}
+          editMode={editMode}
           setEditMode={setEditMode}
           setSelectedActivity={setSelectedActivity}
           createActivity={handleCreateActivity}
           editActivity={handleEditActivity}
-          deleteActivity={handleDeleteActivity} />
+          deleteActivity={handleDeleteActivity}
+          submitting={submitting}
+          target={target} />
       </Container>
 
     </>
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -7,11 +7,12 @@ interface IProps {
     setEditMode: (editMode: boolean) => void,
     activity: IActivity,
     createActivity: (activity: IActivity) => void,
-    editActivity: (activity: IActivity) => void
+    editActivity: (activity: IActivity) => void,
+    submitting: boolean
 }
 
 
-export const ActivityForm: React.FC<IProps> = ({ setEditMode, activity: initialFormState, createActivity, editActivity }) => {
+export const ActivityForm: React.FC<IProps> = ({ setEditMode, activity: initialFormState, createActivity, editActivity, submitting }) => {
 
     const initialForm = () => {
         if (initialFormState) {
@@ -60,7 +61,7 @@ export const ActivityForm: React.FC<IProps> = ({ setEditMode, activity: initialF
                 <Form.Input onChange={handleInputChange} type='datetime-local' placeholder='Date' name='date' value={activity.date} />
                 <Form.Input onChange={handleInputChange} placeholder='City' name='city' value={activity.city} />
                 <Form.Input onChange={handleInputChange} placeholder='Venue' name='venue' value={activity.venue} />
-                <Button floated='right' positive type='submit' content='Submit' />
+                <Button floated='right' loading={submitting} positive type='submit' content='Submit' />
                 <Button onClick={() => setEditMode(false)} floated='right' type='button' content='Cancel' />
             </Form>
         </Segment>
