94cde3e1cd89f49b0b41345a48d2c799f02f7343
Farshid Torkaman <farshidtorkaman@outlook.com>
2020-03-20 20:32:24 +0330

    implement jwt

---
+++ Application/Users/Register.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Errors;
+using Application.Interfaces;
+using Application.Validators;
+using Domain;
+using FluentValidation;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Users
+{
+    public class Register
+    {
+        public class Command : IRequest<User>
+        {
+            public string DisplayName { get; set; }
+            public string Username { get; set; }
+            public string Email { get; set; }
+            public string Password { get; set; }
+        }
+
+        public class CommandValidtor : AbstractValidator<Command>
+        {
+            public CommandValidtor()
+            {
+                RuleFor(f => f.DisplayName).NotEmpty();
+                RuleFor(f => f.Username).NotEmpty();
+                RuleFor(f => f.Email).NotEmpty().EmailAddress();
+                RuleFor(f => f.Password).Password();
+            }
+        }
+
+        public class Handler : IRequestHandler<Command, User>
+        {
+            private readonly DataContext _context;
+            private readonly UserManager<AppUser> _userManager;
+            private readonly IJwtGenerator _jwtGenerator;
+
+            public Handler(DataContext context, UserManager<AppUser> userManager, IJwtGenerator jwtGenerator)
+            {
+                _context = context;
+                _userManager = userManager;
+                _jwtGenerator = jwtGenerator;
+            }
+
+            public async Task<User> Handle(Command request, CancellationToken cancellationToken)
+            {
+                if(await _context.Users.Where(f => f.Email == request.Email).AnyAsync())
+                    throw new RestException(HttpStatusCode.BadRequest, new  { Email = "email already exists" });             
+            
+                if(await _context.Users.Where(f => f.UserName == request.Username).AnyAsync())
+                    throw new RestException(HttpStatusCode.BadRequest, new  { Username = "username already exists" });  
+
+                var user = new AppUser 
+                {
+                    DisplayName = request.DisplayName,
+                    Email = request.Email,
+                    UserName = request.Username
+                };
+
+                var result = await _userManager.CreateAsync(user, request.Password);
+                if (result.Succeeded)
+                {
+                    return new User
+                    {
+                        DisplayName = user.DisplayName,
+                        Token = _jwtGenerator.CreateToken(user),
+                        Username = user.UserName,
+                        Image = null
+                    };
+                }
+
+                throw new Exception("Problem creating user");
+            }
+        }
+    }
+}
---
+++ Infrastructure/Security/UserAccessor.cs
@@ -0,0 +1,24 @@
+using System.Linq;
+using System.Security.Claims;
+using Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+
+namespace Infrastructure.Security
+{
+    public class UserAccessor : IUserAccessor
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public UserAccessor(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public string GetCurrentUsername()
+        {
+            var username = _httpContextAccessor.HttpContext.User?.Claims?
+                .FirstOrDefault(f => f.Type == ClaimTypes.NameIdentifier)?.Value;
+
+            return username;
+        }
+    }
+}
